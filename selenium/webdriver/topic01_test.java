9fe533634ae834e0ff7d47979b8259a0b9f92975
Vomylinh245 <linhvtm.work@gmail.com>
2024-01-14 20:51:52 +0700

    commit

---
+++ selenium/webdriver/topic01_test.java
@@ -0,0 +1,39 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class topic01_test {
+
+    WebDriver driver;
+    @Test
+    public void TC_01_RunOnFirefox() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_01_RunOnChrome() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_01_RunOnEdge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+}
