ae964515c6748679709a88ab8daa04eee2144298
LVOTHIM <ntt.vo_thi_my@extaccount.com>
2024-10-09 18:00:04 +0700

    Commit topic 13

--- selenium/webdriver/topic11_12_Window_Tab.java
+++ selenium/webdriver/topic11_12_Window_Tab.java
@@ -20,88 +20,140 @@ public class topic11_12_Window_Tab {
     }
 
     @Test
-    public void TC_01() throws InterruptedException {
+    public void TC_13_Window_Tab() {
         driver.get("https://automationfc.github.io/basic-form/index.html");
-        // lay ra ID cua tab hien tai - githun
-        String githubWindowID = driver.getWindowHandle();
-        Thread.sleep(3000);
+        //buoc nay can de lam step 10
+        String githubID = driver.getWindowHandle();
 
-        // Step 2.1: click vao GOOGLE
+        //Step 2.1: Click Google link
         driver.findElement(By.xpath("//a[text()='GOOGLE']")).click();
-        Thread.sleep(3000);
 
-        // lay ra tat car window ID
-        Set<String> allWindowID = driver.getWindowHandles();
+        // Step 2.2: Switch qua tab Google
+        switchByTitle("Google");
 
-        // Step 2.2: switch qua tab Google
-        for (String id : allWindowID) {
-            if (!id.equals(githubWindowID)) {
-                driver.switchTo().window(id);
-            }
-        }
-        // Step 3: kiem tra title cua Google
-        Assert.assertEquals(driver.getTitle(), "Google");
+        // Step 4: Switch ve parent window
+        switchByTitle("Selenium WebDriver");
 
-        //Step 4: switch ve tab github
-        String googleWindowID = driver.getWindowHandle();
-        for (String id : allWindowID) {
-            if (!id.equals(googleWindowID)) {
-                driver.switchTo().window(id);
-            }
-        }
-
-        // Step 5.1: click vao Facebook
+        // Step 5.1 CLick Facebook link:
         driver.findElement(By.xpath("//a[text()='FACEBOOK']")).click();
-        Thread.sleep(3000);
 
-        // Step 5.2: switch vao Facebook tab
-        switchToWindowByTitle("Facebook – log in or sign up");
+        // Step 5.2: Switch qua tab Facebook
+         switchByTitle("Log into Facebook");
 
-        // Step 6: kiem tra title cua Facbook
-        Assert.assertEquals(driver.getTitle(), "Facebook – log in or sign up");
+        // Step 6: Kiem tra title cua Facebook
+        Assert.assertEquals(driver.getTitle(), "Log into Facebook");
 
-        //Step 10: close tat ca cac tab ngoai tru parent tab(github)
-        //lay ra toan bo window id
-        Set<String> allWindowId = driver.getWindowHandles();
-        for (String id : allWindowId) {
-            if (!id.equals(githubWindowID)) {
-                driver.switchTo().window(id);
+        // Step 7: Switch ve parent window
+        switchByTitle("Selenium WebDriver");
+
+        // Step 8.1: Click TKI link
+        driver.findElement(By.xpath("//a[text()='TIKI']")).click();
+
+        // Step 8.2: Switch qua tab TIKI
+        switchByTitle("Tiki - Mua hàng online giá tốt, hàng chuẩn, ship nhanh");
+
+        // Step 9: Kiem tra title cua Tiki
+        Assert.assertEquals(driver.getTitle(), "Tiki - Mua hàng online giá tốt, hàng chuẩn, ship nhanh");
+
+        //Step 10: close tat ca tab ngoai tru parent window(github)
+        Set<String> allTabID = driver.getWindowHandles();
+        for (String id : allTabID) {
+            driver.switchTo().window(id);
+            if (!id.equals(githubID)) {
                 driver.close();
             }
         }
     }
 
-    private void switchToWindowByTitle(String expectedPageTitle) {
-        //lay het toan bo ID cua tab: currently co 3 tab la github, google va facebook
-        Set<String> allWindowIDs = driver.getWindowHandles();
-        //dung vong lap duyet qua tung id
-        for (String id : allWindowIDs) {
-            //moi lan duyet cho stwich qua ID truoc
-            driver.switchTo().window(id);
-            //lay ra title cua tab hien tai
-            String pageTitle = driver.getTitle();
-            //kiem tra title
-            if (pageTitle.equals(expectedPageTitle)) {
-                break;
-            }
-        }
+    @Test
+    public void TC_14_Window_Tab() throws InterruptedException {
+        driver.get("http://live.techpanda.org/");
+        // Step 2: Click vao Mobile tab
+        driver.findElement(By.xpath("//a[text()='Mobile']")).click();
+        // Step 3.1: Add to compare SonyXperia
+        driver.findElement(By.xpath("//a[text()='Sony Xperia']/parent::h2/following-sibling::div[@class='actions']//a[text()='Add to Compare']")).click();
+        // Step 3.2: Verify text hien thi
+        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(), "The product Sony Xperia has been added to comparison list.");
+        // Step 4.1: Add to compare Samsung Galaxy
+        driver.findElement(By.xpath("//a[text()='Samsung Galaxy']/parent::h2/following-sibling::div[@class='actions']//a[text()='Add to Compare']")).click();
+        Thread.sleep(3000);
+        // Step 4.2: Verify text hien thi
+        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(), "The product Samsung Galaxy has been added to comparison list.");
+        // Step 5: Click to Compare button
+        driver.findElement(By.xpath("//button[@title='Compare']")).click();
+        Thread.sleep(3000);
+        // Step 6: Switch qua cua so moi
+        switchByTitle("Products Comparison List - Magento Commerce");
+        // Step 7: Verify title cua window
+        Assert.assertEquals(driver.getTitle(), "Products Comparison List - Magento Commerce");
+        //Step 8.1: Close tab
+        driver.close();
+        // Step 8.2: chuyen ve parent window
+        switchByTitle("Mobile");
+        //Step 9: Click Clear All
+        driver.findElement(By.xpath("//a[text()='Clear All']")).click();
+        // Step 10: Accpet Alert
+        driver.switchTo().alert().accept();
+        //Step 11: Verify message
+        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(), "The comparison list was cleared.");
     }
 
     @Test
-    public void TC_01_Window_Tab(){
-        driver.get("https://automationfc.github.io/basic-form/index.html");
-        String githubID = driver.getWindowHandle();
-        driver.findElement(By.xpath("//a[text()='GOOGLE']")).click();
-        switchTabByTitle("Google");
+    public void TC_15_Window_Tab() throws InterruptedException {
+        driver.get("https://dictionary.cambridge.org/vi/");
+        driver.findElement(By.xpath("//span[@class='tb' and text()='Đăng nhập']")).click();
+        // Step 3: switch qua cua so moi
+        switchByTitle("Login");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@value='Log in']")).click();
+        // Step 5: Verify message
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@data-bound-to='loginID' and contains(@class,'gigya-error-type-server')]")).getText(), "This field is required");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@data-bound-to='password' and contains(@class,'gigya-error-type-server')]")).getText(), "This field is required");
+        // Step 6.1: Close Login window
+        driver.close();
+        // Step 6.2: Switch ve trang truoc do
+        switchByTitle("Cambridge Dictionary | Từ điển tiếng Anh, Bản dịch & Từ điển từ đồng nghĩa");
+        // Step 7.1: Nhap tu khoa bat ki vao o search
+        driver.findElement(By.xpath("//input[@placeholder='Tìm kiếm Tiếng Anh']")).sendKeys("automation");
+        // Step 7.2: click vao Search
+        driver.findElement(By.xpath("//button[@aria-label='Search']")).click();
+    }
+
+    @Test
+    public void TC_16_Window_Tab() throws InterruptedException {
+        driver.get("https://courses.dce.harvard.edu/");
+        String parentID = driver.getWindowHandle();
+        driver.findElement(By.xpath("//a[@data-action='login']")).click();
+
+        Thread.sleep(5000);
+
+        Set<String> AllID = driver.getWindowHandles();
+        for(String id: AllID){
+            if(!id.equals(parentID)){
+                driver.switchTo().window(id);
+            }
+        }
+       //driver.findElement(By.xpath("//button[@class='sam-wait__button sam-wait__button--login']")).click();
+
+//        switchByTitle("Harvard Division of Continuing Education Login Portal");
+        Thread.sleep(5000);
+        driver.close();
+        Thread.sleep(5000);
+        switchByTitle("DCE Course Search");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//button[contains(@class,'sam-wait__close')]")).click();
+        // Nhap thong tin hop le vao field
+        driver.findElement(By.xpath("//input[@id='crit-keyword']")).sendKeys("Data Science: An Artificial Ecosystem");
+
 
     }
 
-    private void switchTabByTitle(String expectedTabTitle) {
+    private void switchByTitle(String expectedTabTitle) {
         Set<String> allTabID = driver.getWindowHandles();
-        for(String id: allTabID){
+        for (String id : allTabID) {
             driver.switchTo().window(id);
             String pageTitle = driver.getTitle();
-            if (pageTitle.equals(expectedTabTitle)){
+            if (pageTitle.equals(expectedTabTitle)) {
                 break;
             }
         }
---
+++ selenium/webdriver/topic13__Java_Executor.java
@@ -0,0 +1,144 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Random;
+
+public class topic13__Java_Executor {
+
+    WebDriver driver;
+    JavascriptExecutor jsExecutor;
+    WebDriverWait explicitWait;
+
+    @BeforeClass
+    public void initialBrowser() {
+        driver = new EdgeDriver();
+        jsExecutor = (JavascriptExecutor) driver;
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+    }
+
+
+    @Test
+    public void TC_01_Javascript_Executor() {
+        jsExecutor.executeScript("window.location = 'http://live.techpanda.org/'");
+        //get domain
+        String domain = (String) jsExecutor.executeScript("return document.domain");
+        Assert.assertEquals(domain, "live.techpanda.org");
+        //get URL
+        String URL = (String) jsExecutor.executeScript("return document.URL");
+        Assert.assertEquals(URL, "http://live.techpanda.org/");
+        // click vao Mobile
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//a[text()='Mobile']")));
+        //add Samsung vao card
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//a[text()='Samsung Galaxy']/parent::h2//following-sibling::div[@class='actions']/button")));
+        //verify message hien thi
+        String messageText = jsExecutor.executeScript("return document.documentElement.innerText;").toString();
+        Assert.assertTrue(messageText.contains("Samsung Galaxy was added to your shopping cart."));
+        //click Customer service - verify title
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//a[text()='Customer Service']")));
+        String pageTitle = jsExecutor.executeScript("return document.title;").toString();
+        Assert.assertEquals(pageTitle, "Customer Service");
+        // scroll toi Newsletter textbox
+        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//input[@id='newsletter']")));
+        //Input email hop le
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1])", driver.findElement(By.xpath("//input[@id='newsletter']")), "automation246@gmail.com");
+        // click Submit
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//button[@title='Subscribe']")));
+
+        //verify message
+        String messageText2 = jsExecutor.executeScript("return document.documentElement.innerText;").toString();
+        Assert.assertTrue(messageText2.contains("Thank you for your subscription."));
+    }
+
+    @Test
+    public void TC_02_Javascript_Executor() {
+        jsExecutor.executeScript("window.location = 'https://automationfc.github.io/html5/index.html'");
+        //click vao SUBMIT
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='SUBMIT']")));
+
+        WebElement nameField = driver.findElement(By.xpath("//input[@type='name']"));
+        // verify message o field Name
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].validationMessage;", nameField), "Please fill out this field.");
+        //dien gia tri vaof field Name
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1])", driver.findElement(By.xpath("//input[@type='name']")), "Linh Vo");
+        //click vao Submit
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='SUBMIT']")));
+        //verify message o field password
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].validationMessage;", passwordField), "Please fill out this field.");
+        // dien gia tri vao field password
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1])", driver.findElement(By.xpath("//input[@type='password']")), "abcxyz");
+        //click Submit
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='SUBMIT']")));
+        //verify message o field Email
+        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].validationMessage;", emailField), "Please fill out this field.");
+        //dien gia tri invalid vao field email
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1])", driver.findElement(By.xpath("//input[@type='email']")), "abc.gmail.com");
+        //verify message o field email
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].validationMessage;", emailField), "Please include an '@' in the email address. 'abc.gmail.com' is missing an '@'.");
+        //dien gia tri valid vao field email
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1])", driver.findElement(By.xpath("//input[@type='email']")), "abc@gmail.com");
+        //click Submit
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='SUBMIT']")));
+        WebElement addresslField = driver.findElement(By.xpath("//b[text()='✱ ADDRESS ']/parent::label/following-sibling::select"));
+        //verify message
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].validationMessage;", addresslField), "Please select an item in the list.");
+    }
+
+    @Test
+    public void TC_03_Javascript_Executor() {
+        // Ubuntu
+        jsExecutor.executeScript("window.location = 'https://login.ubuntu.com/'");
+        WebElement emailField = driver.findElement(By.xpath("//form[@id='login-form']//input[@type='email']"));
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1])", emailField, "Linh Vo");
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//button[@data-qa-id='login_button']")));
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].validationMessage;", emailField), "Please include an '@' in the email address. 'Linh Vo' is missing an '@'.");
+        driver.close();
+
+        // Sieuthimaymocthietbi
+        jsExecutor.executeScript("window.location = 'https://sieuthimaymocthietbi.com/account/register'");
+        WebElement lastNameField = driver.findElement(By.xpath("//input[@id='lastName']"));
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//button[@value='Đăng ký']")));
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].validationMessage;", lastNameField), "Please fill out this field.");
+
+    }
+
+    @Test
+    public void TC_04_Javascript_Executor() {
+        jsExecutor.executeScript("window.location = 'http://demo.guru99.com/v4'");
+        String email = "linh" + new Random().nextInt(9000) +"@gmail.com";
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='uid']")), "mngr596627");
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='password']")), "derEgym");
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='LOGIN']")));
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//a[text()='New Customer']")));
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='name']")), "Vo Linh");
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='dob']")), "2000-06-24");
+        jsExecutor.executeScript("arguments[0].value = arguments[1]; arguments[0].onkeyup()", driver.findElement(By.xpath("//textarea[@name='addr']")), "Thanh Xuan");
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='city']")), "Ha Noi");
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='state']")), "Cali");
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='pinno']")), "848484");
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='telephoneno']")), "845454");
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='emailid']")), email);
+        jsExecutor.executeScript("arguments[0].setAttribute('value', arguments[1]); arguments[0].onkeyup()", driver.findElement(By.xpath("//input[@name='password']")), "Abcxyz");
+        jsExecutor.executeScript("arguments[0].click();", driver.findElement(By.xpath("//input[@value='Submit']")));
+        Assert.assertEquals(jsExecutor.executeScript("return arguments[0].innerText;", driver.findElement(By.xpath("//p[@class='heading3']"))), "Customer Registered Successfully!!!");
+
+    }
+
+
+}
